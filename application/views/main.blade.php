0812199d39a3584a4cfa63eb0ee2a40758302613
Niklas Ek <ek.niklas@gmail.com>
2012-09-02 17:27:51 +0200

    HideAddressBar

--- application/views/main.blade.php
+++ application/views/main.blade.php
@@ -6,6 +6,19 @@
 	<title>Bossys</title>
 	<meta name="viewport" content="width=device-width">
 	{{ HTML::style('css/style.css') }}
+	<script>
+		function hideURLbar() {
+			if (window.location.hash.indexOf('#') == -1) {
+				window.scrollTo(0, 1);
+			}
+		}
+
+		if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('Android') != -1) {
+		    addEventListener("load", function() {
+		            setTimeout(hideURLbar, 0);
+		    }, false);
+		}
+	</script>
 </head>
 <body>
 	<div class="wrap">
